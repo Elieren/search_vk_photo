90a3568a2ff3dcae8b880732a4312aba67bc6fd3
recover-zero <kir102906@gmail.com>
2022-09-15 16:58:53 +0700

    Version 2

--- cam_search.py
+++ cam_search.py
@@ -79,9 +79,9 @@ while True:
             name_a = 'Not'
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
-                          (0, 255, 0), cv2.FILLED)
+                          (0, 0, 255), cv2.FILLED)
             cv2.putText(img, name_a, (x1 + 6, y2 - 6),
                         cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
