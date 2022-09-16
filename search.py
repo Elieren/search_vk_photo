3c376bf265f4e18f890556ea5a0d080832893f9c
Elieren <113089080+Elieren@users.noreply.github.com>
2022-09-16 20:23:38 +0700

    V 2.2

--- search.py
+++ search.py
@@ -18,9 +18,9 @@ direct = str(input(": "))
 known_image = face_recognition.load_image_file(direct)
 face_encoding = face_recognition.face_encodings(known_image)[0]
 
-for dirpath, dirnames, filenames in os.walk(".\\id\\"):
+for dirpath, dirnames, filenames in os.walk("./id/"):
     for filename in filenames:
-        fases.append(f'{dirpath}\\{filename}')
+        fases.append(f'{dirpath}/{filename}')
 
 for x in fases:
     unknown_picture = face_recognition.load_image_file(x)
@@ -31,7 +31,7 @@ for x in fases:
         results = face_recognition.compare_faces([face_encoding], encoding)
     
         if results[0] == True:
-            id_p = re.split('\\\\', x)
+            id_p = re.split('/', x)
             p = id_p[2]
             if p not in id_vk:
                 print('+')
